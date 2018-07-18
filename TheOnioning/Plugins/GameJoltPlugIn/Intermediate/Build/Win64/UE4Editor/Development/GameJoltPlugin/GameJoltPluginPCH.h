11a39575c582cfed61214c8e0a7895a6bcbdecfa
EhhhRyan <ryankelly0503@gmail.com>
2018-07-18 00:55:48 -0700

    Added Gamejolt Plugin
    - The plugin wasn't made for 4.18 the author has never updated it apparently so it's disabled but I will try to find a fix.

---
+++ TheOnioning/Plugins/GameJoltPlugIn/Intermediate/Build/Win64/UE4Editor/Development/GameJoltPlugin/GameJoltPluginPCH.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_GAMEJOLTPLUGINPCH_H__
+#define __AUTO_GAMEJOLTPLUGINPCH_H__
+//Last Write: 7/13/2015 5:47:15 PM +00:00
+#include "C:\Users\JvtheWanderer\Desktop\PlugInProject\Plugins\GameJoltPlugIn\Source\GameJoltPlugin\Private\GameJoltPluginPCH.h"
+#endif//__AUTO_GAMEJOLTPLUGINPCH_H__
---
+++ TheOnioning/Plugins/GameJoltPlugIn/Intermediate/Build/Win64/UE4Editor/Development/GameJoltPlugin/PCH.GameJoltPluginPCH.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\JvtheWanderer\Desktop\PlugInProject\Plugins\GameJoltPlugIn\Source\GameJoltPlugin\Private\GameJoltPluginPCH.h"
